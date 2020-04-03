451b7ca150fb00c425479332951c0828b04c504d
William Boakye <wboakye@gmail.com>
2020-04-03 11:37:29 -0400

    stripped

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager
 from flask_heroku import Heroku
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
 """
 DB_STRING = f'postgresql://{os.environ.get("DATABASE_USER", 3)}:{os.environ.get("DATABASE_PASS", 3)}@{os.environ.get("DB_HOST", 3)}/{os.environ.get("DB_NAME", 3)}'
@@ -14,10 +14,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_STRING
 db = SQLAlchemy(app)
 """
-
+"""
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku = Heroku(app)
-db = SQLAlchemy(app)
+
+is_deployed = os.environ.get("IS_DEPLOYED", "")
+
+if not is_deployed:
+    print('Using Localhost Database')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SECRET_KEY'] = os.environ.get("APP_SECRET", 3)
+    db = SQLAlchemy(app)
+else:
+    print("Using Cloud Database")
+    heroku = Heroku(app)
+    db = SQLAlchemy(app)
 
 @app.before_first_request
 def create_tables():
@@ -43,6 +53,14 @@ api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
+"""
+@app.route("/")
+def home_view():
+        return "<h1>APP RUNNING</h1>"
+
+
+
+
 
 if __name__ == ' __main__':
     app.run()
