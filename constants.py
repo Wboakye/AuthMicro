5ced50ca75ede93116664b4d46e916afa913e6c2
William Boakye <wboakye@gmail.com>
2020-04-03 00:08:21 -0400

    fix env

--- constants.py
+++ constants.py
@@ -3,12 +3,12 @@ import config
 """
     APP_SETTINGS: ProductionConfig, StagingConfig, DevelopmentConfig, TestingConfig
 """
-APP_SETTINGS = os.environ["APP_SETTINGS"]
-APP_SECRET = os.environ["APP_SECRET"]
-JWT_SECRET = os.environ["JWT_SECRET"]
-DB_USER = os.environ["DATABASE_USER"]
-DB_PASS = os.environ["DATABASE_PASS"]
-DB_HOST = os.environ["DB_HOST"]
-DB_PORT = os.environ["DB_PORT"]
-DB_NAME = os.environ["DB_NAME"]
+APP_SETTINGS = os.environ.get("APP_SETTINGS", 3)
+APP_SECRET = os.environ.get("APP_SECRET", 3)
+JWT_SECRET = os.environ.get("JWT_SECRET", 3)
+DB_USER = os.environ.get("DATABASE_USER", 3)
+DB_PASS = os.environ.get("DATABASE_PASS", 3)
+DB_HOST = os.environ.get("DB_HOST", 3)
+DB_PORT = os.environ.get("DB_PORT", 3)
+DB_NAME = os.environ.get("DB_NAME", 3)
 DB_STRING = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
