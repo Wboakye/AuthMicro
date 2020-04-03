fba0353ed313e0afafc195c73a7bc70da2f83d43
William Boakye <wboakye@gmail.com>
2020-04-03 02:35:21 -0400

    logger

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from flask_jwt_extended import JWTManager
 import urllib.parse as up
 import psycopg2
 
-
-logger = logging.getLogger()
-
 #from constants import APP_SETTINGS, JWT_SECRET
 from boto.s3.connection import S3Connection
 APP_SETTINGS = os.environ.get("APP_SETTINGS", 3)
@@ -18,16 +15,13 @@ DB_PORT = os.environ.get("DB_PORT", 3)
 DB_STRING = f'postgresql://{os.environ.get("DATABASE_USER", 3)}:{os.environ.get("DATABASE_PASS", 3)}@{os.environ.get("DB_HOST", 3)}/{os.environ.get("DB_NAME", 3)}'
 app = Flask(__name__)
 api = Api(app)
+print("Api Created")
 
 #db_setup
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_STRING
-try:
-    db = SQLAlchemy(app)
-except Exception as e:
-    logger.error("Error creating API", exec_info=True)
-    raise e
-logger.info("Connecte to DB")
+db = SQLAlchemy(app)
+print("DB Connected")
 
 #setup jwt
 app.config['PROPAGATE_EXCEPTIONS'] = True
